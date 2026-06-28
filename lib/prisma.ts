2011e43e87f9c0a028832dadbd9cac0fb0968096
milyas26 <muhammadilyassaya@gmail.com>
2026-06-28 18:44:11 +0700

    feat: pg pool

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,11 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  max: process.env.NODE_ENV === "production" ? 3 : 2,
+  idleTimeoutMillis: 30000,
+})
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
